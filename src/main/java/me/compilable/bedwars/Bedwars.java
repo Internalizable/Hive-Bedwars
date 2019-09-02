48108a3ea46a53ddeda0290b41444ded46049fd2
Internalizable <elietgm1@gmail.com>
2019-09-02 16:06:35 +0300

    Initial commit.

---
+++ src/main/java/me/compilable/bedwars/Bedwars.java
@@ -0,0 +1,16 @@
+package me.compilable.bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bedwars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
